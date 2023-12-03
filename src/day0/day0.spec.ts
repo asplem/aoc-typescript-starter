65a1489f7bac53adbb4f4151cd8ee8f52258cc84
Mari Asplem <mariasplem@gmail.com>
2023-12-03 12:13:21 +0100

    Fix file import in template

--- src/day0/day0.spec.ts
+++ src/day0/day0.spec.ts
@@ -1,5 +1,6 @@
 import day0 from "./index";
 // import fs from "fs";
+// import path from "path";
 
 describe("Day 0", () => {
     it(`solves part 1 with example`, () => {
@@ -17,14 +18,18 @@ describe("Day 0", () => {
     // These are added in case you want to try to refactor your solution after the initial successful solve
     // Which is also why they're initially commented out
     // it(`solves part 1 with real input`, async () => {
-    //     const content = await fs.promises.readFile(`inputs/part1.txt`);
+    //     const content = await fs.promises.readFile(
+    //         path.join(__dirname, `./inputs/part1.txt`),
+    //     );
     //     expect(day0.solveForPartOne(content.toString())).toBe(
     //         "<your correct solution>",
     //     );
     // });
 
     // it(`solves part 2 with real input`, async () => {
-    //     const content = await fs.promises.readFile(`inputs/part2.txt`);
+    //     const content = await fs.promises.readFile(
+    //         path.join(__dirname, `./inputs/part2.txt`),
+    //     );
     //     expect(day0.solveForPartTwo(content.toString())).toBe(
     //         "<your correct solution>",
     //     );
