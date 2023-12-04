bff0668a9135a6b79eaa44a16c858daf94d3965f
Mari Asplem <mariasplem@gmail.com>
2023-12-04 07:34:05 +0100

    Comment out example 2 test

--- src/day0/day0.spec.ts
+++ src/day0/day0.spec.ts
@@ -4,16 +4,12 @@ import day0 from "./index";
 
 describe("Day 0", () => {
     it(`solves part 1 with example`, () => {
-        expect(day0.solveForPartOne("<given example input>")).toBe(
-            "<given solution>",
-        );
+        expect(day0.solveForPartOne(`<given example input>`)).toBe("<given solution>");
     });
 
-    it(`solves part 2 with example`, () => {
-        expect(day0.solveForPartTwo("<given example input>")).toBe(
-            "<given solution>",
-        );
-    });
+    // it(`solves part 2 with example`, () => {
+    //     expect(day0.solveForPartTwo(`<given example input>`)).toBe("<given solution>");
+    // });
 
     // These are added in case you want to try to refactor your solution after the initial successful solve
     // Which is also why they're initially commented out
